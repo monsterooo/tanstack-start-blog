239666f13b759321eb9fd0d13b535e8dcc6520c9
monsterooo <createthink.net@gmail.com>
2025-11-01 22:40:48 +0800

    home page

--- src/config/site-metadata.ts
+++ src/config/site-metadata.ts
@@ -1,5 +1,8 @@
+import { enUS } from 'date-fns/locale' // 导入对应 locale
+
 export const siteMetadata = {
   stickyNav: false,
   headerTitle: 'TanStack Blog',
   description: 'A blog created with Tanstack Start and Tailwind.css',
+  locale: enUS,
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
 import { siteMetadata } from '@/config/site-metadata'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { allPosts } from 'content-collections'
+import { format } from 'date-fns'
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({
+  component: App,
+  loader: () => allPosts.filter((blog) => blog.published),
+})
+
+const MAX_DISPLAY = 5
 
 function App() {
+  const posts = Route.useLoaderData()
+
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
@@ -14,6 +23,65 @@ function App() {
           {siteMetadata.description}
         </p>
       </div>
+      <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        {!posts.length && 'No posts found.'}
+
+        {posts.slice(0, MAX_DISPLAY).map((post) => (
+          <li key={post.slug} className="py-12">
+            <article>
+              <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                <dl>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
+                    <time dateTime={post.publishedAt}>
+                      {format(post.publishedAt, 'MMMM dd, yyyy', {
+                        locale: siteMetadata.locale,
+                      })}
+                    </time>
+                  </dd>
+                </dl>
+
+                <div className="space-y-5 xl:col-span-3">
+                  <div className="space-y-6">
+                    <div>
+                      <h2 className="text-2xl leading-8 font-bold tracking-tight">
+                        <Link
+                          to={`/`}
+                          className="text-gray-900 dark:text-gray-100"
+                        >
+                          {post.title}
+                        </Link>
+                      </h2>
+                      <div className="flex flex-wrap">
+                        {post.tags?.map((tag) => (
+                          <Link
+                            to={`/`}
+                            className="text-primary hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase"
+                          >
+                            {tag.split(' ').join('-')}
+                          </Link>
+                        ))}
+                      </div>
+                      <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        {post.summary}
+                      </div>
+                    </div>
+                    <div className="text-base leading-6 font-medium">
+                      <Link
+                        to={`/`}
+                        className="text-primary hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read more: "${post.title}"`}
+                      >
+                        Read more &rarr;
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
